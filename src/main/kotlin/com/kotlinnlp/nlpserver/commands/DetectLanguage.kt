46630f0f8d64c70929024cf19fb4451bc083f67b
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-08 16:39:43 +0200

    Update invoke() return value and update documentation

--- src/main/kotlin/com/kotlinnlp/nlpserver/commands/DetectLanguage.kt
+++ src/main/kotlin/com/kotlinnlp/nlpserver/commands/DetectLanguage.kt
@@ -7,12 +7,15 @@
 
 package com.kotlinnlp.nlpserver.commands
 
+import com.beust.klaxon.JsonArray
 import com.beust.klaxon.json
 import com.kotlinnlp.languagedetector.LanguageDetector
 import com.kotlinnlp.languagedetector.LanguageDetectorModel
 import com.kotlinnlp.languagedetector.utils.FrequencyDictionary
+import com.kotlinnlp.languagedetector.utils.Language
 import com.kotlinnlp.languagedetector.utils.TextTokenizer
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizerModel
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
 import java.io.File
 import java.io.FileInputStream
 import java.util.logging.Logger
@@ -61,45 +64,98 @@ class DetectLanguage(modelFilename: String, cjkModelFilename: String, frequencyD
 
   /**
    * Detect the language of the given [text].
+   * The return value is A JSON string containing the detected language iso-a2 code and the complete classification.
+   *
+   * The template of the JSON object:
+   *  {
+   *    "language": <STRING>, // iso-a2 code
+   *    "classification": {
+   *      "en": <DOUBLE>,
+   *      "ar": <DOUBLE>,
+   *      ...
+   *    }
+   *  }
    *
    * @param text the input text
    *
-   * @return the isoA2 code of the detected language
+   * @return a [String] with a JSON object containing the detected language iso-a2 code and the complete classification
    */
   operator fun invoke(text: String): String {
-    return this.languageDetector.detectLanguage(text).isoCode
+
+    val prediction: DenseNDArray = this.languageDetector.predict(text)
+    val language: Language = this.languageDetector.extractLanguage(prediction)
+
+    return json {
+      obj(
+        "language" to language.isoCode,
+        "classification" to prediction.toLanguageScorePairs()
+      )
+    }.toJsonString()
   }
 
   /**
    * Classify the language for each token of the given [text].
+   *
    * The return value is A JSON string containing a list of token_classifications.
    * Each token token_classification is a list containing the token itself as first element and its classification as
    * second element.
    * The classification is an object containing the probability of each language mapped to its iso-code.
    *
+   * The template of the JSON object:
+   *  {
+   *    "word": <STRING>,
+   *    "classification": {
+   *      "languages": {
+   *        "en": <DOUBLE>,
+   *        "ar": <DOUBLE>,
+   *        ...
+   *      },
+   *      "charsImportance": [<DOUBLE>, <DOUBLE>, ...] // same length of the token
+   *    }
+   *  }
+   *
    * @param text the input text
    *
-   * @return a JSON list containing the language classification of each token
+   * @return a [String] with a JSON list containing the language classification of each token
    */
   fun perToken(text: String): String {
 
     val tokensClassifications: List<Pair<String, LanguageDetector.TokenClassification>>
       = this.languageDetector.classifyTokens(text)
 
-    val languages = this.languageDetector.model.supportedLanguages
-
     return json {
       array(tokensClassifications.map {
         obj(
           "word" to it.first,
           "classification" to obj(
-            "languages" to obj(*it.second.languages.toDoubleArray().mapIndexed { i, score ->
-              Pair(languages[i].isoCode, score)
-            }.toTypedArray()),
-            "charsImportance" to array(it.second.charsImportance.toDoubleArray().toList())
+            "languages" to obj(*it.second.languages.toLanguageScorePairs()),
+            "charsImportance" to it.second.charsImportance.toJSONArray()
           )
         )
       })
     }.toJsonString()
+  }
+
+  /**
+   *
+   */
+  private fun DenseNDArray.toJSONArray(): JsonArray<Any?> = json { array(this@toJSONArray.toDoubleArray().toList()) }
+
+  /**
+   * Convert a [DenseNDArray] representing a languages classification to an [Array] of [Pair]s <isoA2-code, score>.
+   *
+   * @return an [Array] of [Pair]s <isoA2-code, score>
+   */
+  private fun DenseNDArray.toLanguageScorePairs(): Array<Pair<String, Double>> {
+
+    val languages = this@DetectLanguage.languageDetector.model.supportedLanguages
+
+    require(this.length == languages.size) {
+      "Invalid this (length %d, supported languages %d)".format(this.length, languages.size)
+    }
+
+    return this.toDoubleArray().mapIndexed { i, score ->
+      Pair(languages[i].isoCode, score)
+    }.toTypedArray()
   }
 }
