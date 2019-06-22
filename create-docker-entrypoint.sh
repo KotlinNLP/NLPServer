dba9056ac525b957da4d8abff58325cb4d0116c7
KotlinNLP Authors <github@kotlinnlp.com>
2019-06-22 16:15:27 +0200

    Code refactoring

--- create-docker-entrypoint.sh
+++ create-docker-entrypoint.sh
@@ -14,70 +14,70 @@ locations_dictionary="models/locations_dictionary.serialized"
 
 parameters="-p $1"
 
-if [[ -d $tokenizer_models_directory ]] && [[ -n $(ls -A $tokenizer_models_directory) ]]; then
+if [[ -d "$tokenizer_models_directory" ]] && [[ -n $(ls -A "$tokenizer_models_directory") ]]; then
     echo "Loaded tokenizers:"
-    ls -1 $tokenizer_models_directory
-    parameters=$parameters" -t $tokenizer_models_directory"
+    ls -1 ${tokenizer_models_directory}
+    parameters="$parameters -t $tokenizer_models_directory"
 fi
 
 if [[ -f "$cjk_tokenizer_model" ]]; then
     echo "Loaded cjk tokenizer:"
-    echo $cjk_tokenizer_model
-    parameters=$parameters" -c $cjk_tokenizer_model"
+    echo ${cjk_tokenizer_model}
+    parameters="$parameters -c $cjk_tokenizer_model"
 fi
 
 if [[ -f "$language_detector_model" ]]; then
     echo "Loaded language detector:"
-    echo $language_detector_model
-    parameters=$parameters" -l $language_detector_model"
+    echo ${language_detector_model}
+    parameters="$parameters -l $language_detector_model"
 fi
 
 if [[ -f "$frequency_dictionary" ]]; then
     echo "Loaded frequency dictionary:"
-    echo $frequency_dictionary
-    parameters=$parameters" -f $frequency_dictionary"
+    echo ${frequency_dictionary}
+    parameters="$parameters -f $frequency_dictionary"
 fi
 
-if [[ -d $morphology_dictionaries ]] && [[ -n $(ls -A $morphology_dictionaries) ]]; then
+if [[ -d "$morphology_dictionaries" ]] && [[ -n $(ls -A "$morphology_dictionaries") ]]; then
     echo "Loaded morphology dictionaries:"
-    ls -1 $morphology_dictionaries
-    parameters=$parameters" -m $morphology_dictionaries"
+    ls -1 ${morphology_dictionaries}
+    parameters="$parameters -m $morphology_dictionaries"
 fi
 
-if [[ -d $neural_parsers ]] && [[ -n $(ls -A $neural_parsers) ]]; then
+if [[ -d "$neural_parsers" ]] && [[ -n $(ls -A "$neural_parsers") ]]; then
     echo "Loaded neural parsers:"
-    ls -1 $neural_parsers
-    parameters=$parameters" -n $neural_parsers"
+    ls -1 ${neural_parsers}
+    parameters="$parameters -n $neural_parsers"
 fi
 
-if [[ -d $frame_extractors ]] && [[ -n $(ls -A $frame_extractors) ]]; then
+if [[ -d "$frame_extractors" ]] && [[ -n $(ls -A "$frame_extractors") ]]; then
     echo "Loaded frame extractors:"
-    ls -1 $frame_extractors
-    parameters=$parameters" -x $frame_extractors"
+    ls -1 ${frame_extractors}
+    parameters="$parameters -x $frame_extractors"
 fi
 
-if [[ -d $frame_extractor_embeddings ]] && [[ -n $(ls -A $frame_extractor_embeddings) ]]; then
+if [[ -d "$frame_extractor_embeddings" ]] && [[ -n $(ls -A "$frame_extractor_embeddings") ]]; then
     echo "Loaded frame extractors embeddings:"
-    ls -1 $frame_extractor_embeddings
-    parameters=$parameters" -e $frame_extractor_embeddings"
+    ls -1 ${frame_extractor_embeddings}
+    parameters="$parameters -e $frame_extractor_embeddings"
 fi
 
-if [[ -d $han_classifiers ]] && [[ -n $(ls -A $han_classifiers) ]]; then
+if [[ -d "$han_classifiers" ]] && [[ -n $(ls -A "$han_classifiers") ]]; then
     echo "Loaded han classifiers:"
-    ls -1 $han_classifiers
-    parameters=$parameters" -s $han_classifiers"
+    ls -1 ${han_classifiers}
+    parameters="$parameters -s $han_classifiers"
 fi
 
-if [[ -d $han_classifier_embeddings ]] && [[ -n $(ls -A $han_classifier_embeddings) ]]; then
+if [[ -d "$han_classifier_embeddings" ]] && [[ -n $(ls -A "$han_classifier_embeddings") ]]; then
     echo "Loaded han classifiers embeddings:"
-    ls -1 $han_classifier_embeddings
-    parameters=$parameters" -g $han_classifier_embeddings"
+    ls -1 ${han_classifier_embeddings}
+    parameters="$parameters -g $han_classifier_embeddings"
 fi
 
 if [[ -f "$locations_dictionary" ]]; then
     echo "Loaded locations dictionary:"
-    echo $locations_dictionary
-    parameters=$parameters" -d $locations_dictionary"
+    echo ${locations_dictionary}
+    parameters="$parameters -d $locations_dictionary"
 fi
 
 echo "#!/bin/bash" > docker-entrypoint.sh
