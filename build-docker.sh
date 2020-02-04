1da235dea57b12a764e569d33cdf351a2ed9d4da
KotlinNLP Authors <github@kotlinnlp.com>
2020-02-04 09:14:01 +0100

    Code refactoring

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 REV=$(git rev-parse --short HEAD)
-NLP_IMAGE=nlp-server:$REV
+NLP_IMAGE=nlp-server:${REV}
 PORT=3000
 
 sudo docker build --tag ${NLP_IMAGE} --build-arg PORT=${PORT} .
