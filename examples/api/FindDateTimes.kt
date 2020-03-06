8927f75210f6f6f4856a58de66a2a430c3aef2bd
KotlinNLP Authors <github@kotlinnlp.com>
2020-03-06 12:14:54 +0100

    Add example FindDateTimes.kt

---
+++ examples/api/FindDateTimes.kt
@@ -0,0 +1,48 @@
+/* Copyright 2020-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package api
+
+import com.kotlinnlp.ApiClient
+import com.kotlinnlp.api.NlpServiceApi
+import com.kotlinnlp.api.model.DateTimeExpr
+import com.kotlinnlp.api.model.InputText
+
+/**
+ * Test the `getDateTimes` method of the KotlinNLP APIs.
+ *
+ * Launch with the '-h' option for help about the command line arguments.
+ */
+fun main(args: Array<String>) {
+
+  val parsedArgs = CommandLineArguments(args)
+  val client = NlpServiceApi(ApiClient().setBasePath("http://${parsedArgs.host}:${parsedArgs.port}"))
+
+  while (true) {
+
+    readInput()?.let {
+
+      val dateTimes: List<DateTimeExpr> = client.getDateTimes(InputText().text(it), false)
+
+      println("\nDate-time expressions found:")
+      println(dateTimes)
+
+    } ?: break
+  }
+}
+
+/**
+ * Read a text from the standard input.
+ *
+ * @return the string read or null if it was empty
+ */
+private fun readInput(): String? {
+
+  print("\nInput text (empty to exit): ")
+
+  return readLine()!!.trim().ifEmpty { null }
+}
