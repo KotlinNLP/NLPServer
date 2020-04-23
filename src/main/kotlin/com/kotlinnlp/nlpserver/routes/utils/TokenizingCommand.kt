e93ccdd1509e1778374f459ffcf927fe12b3f8e0
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-04-23 21:30:23 +0200

    Add method tokenize()

--- src/main/kotlin/com/kotlinnlp/nlpserver/routes/utils/TokenizingCommand.kt
+++ src/main/kotlin/com/kotlinnlp/nlpserver/routes/utils/TokenizingCommand.kt
@@ -9,6 +9,7 @@ package com.kotlinnlp.nlpserver.routes.utils
 
 import com.kotlinnlp.linguisticdescription.language.Language
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizer
+import com.kotlinnlp.neuraltokenizer.Sentence
 import com.kotlinnlp.nlpserver.LanguageNotSupported
 
 /**
@@ -33,5 +34,27 @@ internal interface TokenizingCommand : LanguageDetectingCommand {
    */
   override fun checkLanguage(language: Language?): Language = super.checkLanguage(language).also {
     if (it.isoCode !in this.tokenizers) throw LanguageNotSupported(it.isoCode)
+  }
+
+  /**
+   * Tokenize a given text.
+   * If a [language] is given the related tokenizer is forced to be used, otherwise the [languageDetector] is used to
+   * choose the right tokenizer.
+   *
+   * @param text the input text
+   * @param language the language with which to force the tokenization or null to detect it automatically
+   *
+   * @return the text split in sentences and tokens
+   */
+  fun tokenize(text: String, language: Language? = null): List<Sentence> {
+
+    val tokenizerLang: Language = this.getTextLanguage(text = text, forcedLang = language)
+
+    if (tokenizerLang.isoCode !in this.tokenizers)
+      throw LanguageNotSupported(tokenizerLang.isoCode)
+
+    this.logger.debug("Tokenizing text '${text.cutText(50)}'...")
+
+    return this.tokenizers.getValue(tokenizerLang.isoCode).tokenize(text)
   }
 }
